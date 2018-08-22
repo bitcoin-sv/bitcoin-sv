bf5d3a375dfe1c8e5c514174d7570326b2ae036c
Richard Mills <richardm@ncrypt.com>
2018-08-22 15:44:49 +0100

    Add functional tests.
    Add a couple of new functional tests to check for being able to
    set the block size flags via the bitcoind.conf file and to check
    we can actually process 128M blocks.

---
+++ test/functional/bsv-128Mb-blocks.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+# Copyright (c) 2015-2016 The Bitcoin Core developers
+# Copyright (c) 2017 The Bitcoin developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""
+This test checks that by using the newly promoted -excessiveblocksize
+flag we can actually process very big blocks.
+"""
+
+from test_framework.test_framework import ComparisonTestFramework
+from test_framework.util import assert_equal, assert_raises_rpc_error
+from test_framework.comptool import TestManager, TestInstance, RejectResult
+from test_framework.blocktools import *
+import time
+from test_framework.script import *
+from test_framework.cdefs import (ONE_MEGABYTE, MAX_TX_SIGOPS_COUNT)
+from collections import deque
+
+
+class PreviousSpendableOutput():
+
+    def __init__(self, tx=CTransaction(), n=-1):
+        self.tx = tx
+        self.n = n  # the output we're spending
+
+
+class BSV128MBlocks(ComparisonTestFramework):
+
+    def set_test_params(self):
+        self.num_nodes = 1
+        self.setup_clean_chain = True
+        self.block_heights = {}
+        self.tip = None
+        self.blocks = {}
+        self.excessive_block_size = 128 * ONE_MEGABYTE
+        self.extra_args = [['-whitelist=127.0.0.1',
+                            "-excessiveblocksize=%d" % self.excessive_block_size]]
+
+    def add_options(self, parser):
+        super().add_options(parser)
+
+    def run_test(self):
+        self.test = TestManager(self, self.options.tmpdir)
+        self.test.add_all_connections(self.nodes)
+        # Start up network handling in another thread
+        NetworkThread().start()
+        # Set the blocksize to 2MB as initial condition
+        self.nodes[0].setexcessiveblock(self.excessive_block_size)
+        self.test.run()
+
+    def add_transactions_to_block(self, block, tx_list):
+        [tx.rehash() for tx in tx_list]
+        block.vtx.extend(tx_list)
+
+    def next_block(self, number, spend=None, script=CScript([OP_TRUE]), block_size=0, extra_sigops=0):
+        if self.tip == None:
+            base_block_hash = self.genesis_hash
+            block_time = int(time.time()) + 1
+        else:
+            base_block_hash = self.tip.sha256
+            block_time = self.tip.nTime + 1
+        # First create the coinbase
+        height = self.block_heights[base_block_hash] + 1
+        coinbase = create_coinbase(height)
+        coinbase.rehash()
+        if spend == None:
+            # We need to have something to spend to fill the block.
+            assert_equal(block_size, 0)
+            block = create_block(base_block_hash, coinbase, block_time)
+        else:
+            # all but one satoshi to fees
+            coinbase.vout[0].nValue += spend.tx.vout[spend.n].nValue - 1
+            coinbase.rehash()
+            block = create_block(base_block_hash, coinbase, block_time)
+
+            # Make sure we have plenty engough to spend going forward.
+            spendable_outputs = deque([spend])
+
+            def get_base_transaction():
+                # Create the new transaction
+                tx = CTransaction()
+                # Spend from one of the spendable outputs
+                spend = spendable_outputs.popleft()
+                tx.vin.append(CTxIn(COutPoint(spend.tx.sha256, spend.n)))
+                # Add spendable outputs
+                for i in range(4):
+                    tx.vout.append(CTxOut(0, CScript([OP_TRUE])))
+                    spendable_outputs.append(PreviousSpendableOutput(tx, i))
+                return tx
+
+            tx = get_base_transaction()
+
+            # Make it the same format as transaction added for padding and save the size.
+            # It's missing the padding output, so we add a constant to account for it.
+            tx.rehash()
+            base_tx_size = len(tx.serialize()) + 18
+
+            # If a specific script is required, add it.
+            if script != None:
+                tx.vout.append(CTxOut(1, script))
+
+            # Put some random data into the first transaction of the chain to randomize ids.
+            tx.vout.append(
+                CTxOut(0, CScript([random.randint(0, 256), OP_RETURN])))
+
+            # Add the transaction to the block
+            self.add_transactions_to_block(block, [tx])
+
+            # If we have a block size requirement, just fill
+            # the block until we get there
+            current_block_size = len(block.serialize())
+            while current_block_size < block_size:
+                # We will add a new transaction. That means the size of
+                # the field enumerating how many transaction go in the block
+                # may change.
+                current_block_size -= len(ser_compact_size(len(block.vtx)))
+                current_block_size += len(ser_compact_size(len(block.vtx) + 1))
+
+                # Create the new transaction
+                tx = get_base_transaction()
+
+                # Add padding to fill the block.
+                script_length = block_size - current_block_size - base_tx_size
+                if script_length > 510000:
+                    if script_length < 1000000:
+                        # Make sure we don't find ourselves in a position where we
+                        # need to generate a transaction smaller than what we expected.
+                        script_length = script_length // 2
+                    else:
+                        script_length = 500000
+                tx_sigops = min(extra_sigops, script_length,
+                                MAX_TX_SIGOPS_COUNT)
+                extra_sigops -= tx_sigops
+                script_pad_len = script_length - tx_sigops
+                script_output = CScript(
+                    [b'\x00' * script_pad_len] + [OP_CHECKSIG] * tx_sigops)
+                tx.vout.append(CTxOut(0, script_output))
+
+                # Add the tx to the list of transactions to be included
+                # in the block.
+                self.add_transactions_to_block(block, [tx])
+                current_block_size += len(tx.serialize())
+
+            # Now that we added a bunch of transaction, we need to recompute
+            # the merkle root.
+            block.hashMerkleRoot = block.calc_merkle_root()
+
+        # Check that the block size is what's expected
+        if block_size > 0:
+            assert_equal(len(block.serialize()), block_size)
+
+        # Do PoW, which is cheap on regnet
+        block.solve()
+        self.tip = block
+        self.block_heights[block.sha256] = height
+        assert number not in self.blocks
+        self.blocks[number] = block
+        return block
+
+    def get_tests(self):
+        node = self.nodes[0]
+        self.genesis_hash = int(node.getbestblockhash(), 16)
+        self.block_heights[self.genesis_hash] = 0
+        spendable_outputs = []
+
+        # save the current tip so it can be spent by a later block
+        def save_spendable_output():
+            spendable_outputs.append(self.tip)
+
+        # get an output that we previously marked as spendable
+        def get_spendable_output():
+            return PreviousSpendableOutput(spendable_outputs.pop(0).vtx[0], 0)
+
+        # returns a test case that asserts that the current tip was accepted
+        def accepted():
+            return TestInstance([[self.tip, True]])
+
+        # returns a test case that asserts that the current tip was rejected
+        def rejected(reject=None):
+            if reject is None:
+                return TestInstance([[self.tip, False]])
+            else:
+                return TestInstance([[self.tip, reject]])
+
+        # move the tip back to a previous block
+        def tip(number):
+            self.tip = self.blocks[number]
+
+        # adds transactions to the block and updates state
+        def update_block(block_number, new_transactions):
+            block = self.blocks[block_number]
+            self.add_transactions_to_block(block, new_transactions)
+            old_sha256 = block.sha256
+            block.hashMerkleRoot = block.calc_merkle_root()
+            block.solve()
+            # Update the internal state just like in next_block
+            self.tip = block
+            if block.sha256 != old_sha256:
+                self.block_heights[
+                    block.sha256] = self.block_heights[old_sha256]
+                del self.block_heights[old_sha256]
+            self.blocks[block_number] = block
+            return block
+
+        # shorthand for functions
+        block = self.next_block
+
+        # Create a new block
+        block(0)
+        save_spendable_output()
+        yield accepted()
+
+        # Now we need that block to mature so we can spend the coinbase.
+        test = TestInstance(sync_every_block=False)
+        for i in range(99):
+            block(5000 + i)
+            test.blocks_and_transactions.append([self.tip, True])
+            save_spendable_output()
+        yield test
+
+        # collect spendable outputs now to avoid cluttering the code later on
+        out = []
+        for i in range(100):
+            out.append(get_spendable_output())
+
+        # block of maximal size
+        block(1, spend=out[0], block_size=self.excessive_block_size)
+        yield accepted()
+
+        # Reject oversized blocks with bad-blk-length error
+        block(2, spend=out[1], block_size=self.excessive_block_size + 1)
+        yield rejected(RejectResult(16, b'bad-blk-length'))
+
+
+if __name__ == '__main__':
+    BSV128MBlocks().main()
---
+++ test/functional/bsv-blocksize-params.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Copyright (c) 2015-2016 The Bitcoin Core developers
+# Copyright (c) 2017 The Bitcoin developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""
+Test that the blockmaxsize and excessiveblocksize parameters are also
+settable via the bitcoin.conf file.
+"""
+
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import assert_equal, assert_raises_rpc_error
+from test_framework.cdefs import (ONE_MEGABYTE)
+
+import os
+
+class BSVBlockSizeParams(BitcoinTestFramework):
+
+    def set_test_params(self):
+        self.num_nodes = 1
+        self.maxminedblocksize = 4 * ONE_MEGABYTE
+        self.maxblocksize = 16 * ONE_MEGABYTE
+
+    def setup_chain(self):
+        super().setup_chain()
+        with open(os.path.join(self.options.tmpdir + "/node0", "bitcoin.conf"), 'a', encoding='utf8') as f:
+            f.write("blockmaxsize=" + str(self.maxminedblocksize) + "\n")
+            f.write("excessiveblocksize=" + str(self.maxblocksize) + "\n")
+
+    def add_options(self, parser):
+        super().add_options(parser)
+
+    def run_test(self):
+        gires = self.nodes[0].getinfo()
+        assert_equal(gires["maxblocksize"], self.maxblocksize)
+        assert_equal(gires["maxminedblocksize"], self.maxminedblocksize)
+
+
+if __name__ == '__main__':
+    BSVBlockSizeParams().main()
