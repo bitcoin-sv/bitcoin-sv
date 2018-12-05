bdcc496159b2893f6f78aede18254e2811f8ef87
John <j.murphy@nchain.com>
2018-12-05 13:52:48 +0000

    Deserialized key should be the correct size.

--- src/key.h
+++ src/key.h
@@ -170,6 +170,8 @@ struct CExtKey {
     template <typename Stream> void Unserialize(Stream &s) {
         unsigned int len = ::ReadCompactSize(s);
         uint8_t code[BIP32_EXTKEY_SIZE];
+        if (len != BIP32_EXTKEY_SIZE)
+            throw std::runtime_error("Invalid extended key size\n");
         s.read((char *)&code[0], len);
         Decode(code);
     }
