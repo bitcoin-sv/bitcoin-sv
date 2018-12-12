610144af176c6b7d8e0d8f8a0738eeeb2d66211e
John <j.murphy@nchain.com>
2018-12-12 17:36:06 +0000

    Possible NULL pointer dereference in LevelDB CopyString
    Add a simple null pointer check.

--- src/leveldb/db/c.cc
+++ src/leveldb/db/c.cc
@@ -145,7 +145,8 @@ static bool SaveError(char** errptr, const Status& s) {
 
 static char* CopyString(const std::string& str) {
   char* result = reinterpret_cast<char*>(malloc(sizeof(char) * str.size()));
-  memcpy(result, str.data(), sizeof(char) * str.size());
+  if (result != nullptr)
+      memcpy(result, str.data(), sizeof(char) * str.size());
   return result;
 }
 
