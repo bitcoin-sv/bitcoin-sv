159a0f5acc535254af4f2383b3e276aa1ae3407c
Daniel Connolly <d.connolly@nchain.com>
2019-04-30 23:26:53 +0200

    Replace more references to BCH with BSV
    With thanks to gitzhou@github

--- src/amount.cpp
+++ src/amount.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2016 The Bitcoin Core developers
+// Copyright (c) 2019 The Bitcoin SV developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -7,7 +8,7 @@
 
 #include "tinyformat.h"
 
-const std::string CURRENCY_UNIT = "BCH";
+const std::string CURRENCY_UNIT = "BSV";
 
 std::string Amount::ToString() const {
     return strprintf("%d.%08d %s", amount / COIN.GetSatoshis(),
