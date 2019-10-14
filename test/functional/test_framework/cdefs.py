681870d5d4a46ee3186b9ca4b3e0daf1fb4bbf5d
Boštjan Vižin <bostjan.vizin@crea.si>
2019-10-14 13:46:53 +0200

    Fixed functional tests

--- test/functional/test_framework/cdefs.py
+++ test/functional/test_framework/cdefs.py
@@ -19,6 +19,13 @@ _policy_h_fh = open(os.path.join(get_srcdir(), 'src', 'policy',
 _policy_h_contents = _policy_h_fh.read()
 _policy_h_fh.close()
 
+# Slurp in chainparams.cpp contents
+_chainparams_cpp_fh = open(os.path.join(get_srcdir(), 'src', 
+                                    'chainparams.cpp'), 'rt')
+_chainparams_cpp_contents = _chainparams_cpp_fh.read()
+_chainparams_cpp_fh.close()
+
+
 # This constant is currently needed to evaluate some that are formulas
 ONE_MEGABYTE = 1000000
 ONE_GIGABYTE = 1000000000
@@ -26,6 +33,8 @@ ONE_GIGABYTE = 1000000000
 def _extractPolicyValue(name):
     return eval(re.search(name + ' = (.+);', _policy_h_contents).group(1))
 
+def _extractChainParamsValue(name):
+    return eval(re.search('#define ' + name + ' (.+)', _chainparams_cpp_contents).group(1))
 
 # Extract relevant default values parameters
 
@@ -34,12 +43,14 @@ LEGACY_MAX_BLOCK_SIZE = ONE_MEGABYTE
 
 # Default settings for maximum allowed size for a block, in bytes - berfore and after activation of new rules
 REGTEST_NEW_BLOCKSIZE_ACTIVATION_TIME = _extractPolicyValue('REGTEST_NEW_BLOCKSIZE_ACTIVATION_TIME')
-REGTEST_DEFAULT_MAX_BLOCK_SIZE_BEFORE = _extractPolicyValue('REGTEST_DEFAULT_MAX_BLOCK_SIZE_BEFORE')
-REGTEST_DEFAULT_MAX_BLOCK_SIZE_AFTER =  _extractPolicyValue('REGTEST_DEFAULT_MAX_BLOCK_SIZE_AFTER')
+REGTEST_DEFAULT_MAX_BLOCK_SIZE_BEFORE_GENESIS = _extractPolicyValue('REGTEST_DEFAULT_MAX_BLOCK_SIZE_BEFORE_GENESIS')
+REGTEST_DEFAULT_MAX_BLOCK_SIZE_AFTER_GENESIS =  _extractPolicyValue('REGTEST_DEFAULT_MAX_BLOCK_SIZE_AFTER_GENESIS')
 REGTEST_DEFAULT_MAX_GENERATED_BLOCK_SIZE_BEFORE = _extractPolicyValue('REGTEST_DEFAULT_MAX_GENERATED_BLOCK_SIZE_BEFORE')
 REGTEST_DEFAULT_MAX_GENERATED_BLOCK_SIZE_AFTER = _extractPolicyValue('REGTEST_DEFAULT_MAX_GENERATED_BLOCK_SIZE_AFTER')
 
 MAX_STANDARD_TX_SIZE = _extractPolicyValue('MAX_STANDARD_TX_SIZE')
+
+GENESIS_ACTIVATION_HEIGHT_REGTEST = _extractChainParamsValue('GENESIS_ACTIVATION_REGTEST')
 
 # The following consensus parameters should not be automatically imported.
 # They *should* cause test failures if application code is changed in ways
@@ -68,11 +79,12 @@ MAX_TX_SIZE = ONE_MEGABYTE
 if __name__ == "__main__":
     # Output values if run standalone to verify
     print("REGTEST_NEW_BLOCKSIZE_ACTIVATION_TIME = %d" % REGTEST_NEW_BLOCKSIZE_ACTIVATION_TIME)
-    print("REGTEST_DEFAULT_MAX_BLOCK_SIZE_BEFORE = %d (bytes)" % REGTEST_DEFAULT_MAX_BLOCK_SIZE_BEFORE)
-    print("REGTEST_DEFAULT_MAX_BLOCK_SIZE_AFTER = %d (bytes)" % REGTEST_DEFAULT_MAX_BLOCK_SIZE_AFTER)
+    print("REGTEST_DEFAULT_MAX_BLOCK_SIZE_BEFORE_GENESIS = %d (bytes)" % REGTEST_DEFAULT_MAX_BLOCK_SIZE_BEFORE_GENESIS)
+    print("REGTEST_DEFAULT_MAX_BLOCK_SIZE_AFTER_GENESIS = %d (bytes)" % REGTEST_DEFAULT_MAX_BLOCK_SIZE_AFTER_GENESIS)
     print("REGTEST_DEFAULT_MAX_GENERATED_BLOCK_SIZE_BEFORE = %d (bytes)" % REGTEST_DEFAULT_MAX_GENERATED_BLOCK_SIZE_BEFORE)
     print("REGTEST_DEFAULT_MAX_GENERATED_BLOCK_SIZE_AFTER = %d (bytes)" % REGTEST_DEFAULT_MAX_GENERATED_BLOCK_SIZE_AFTER)
     print("MAX_BLOCK_SIGOPS_PER_MB = %d (sigops)" % MAX_BLOCK_SIGOPS_PER_MB)
     print("MAX_TX_SIGOPS_COUNT = %d (sigops)" % MAX_TX_SIGOPS_COUNT)
     print("COINBASE_MATURITY = %d (blocks)" % COINBASE_MATURITY)
     print("MAX_STANDARD_TX_SIZE = %d" % MAX_STANDARD_TX_SIZE)
+    print("GENESIS_ACTIVATION_HEIGHT_REGTEST = %d" % GENESIS_ACTIVATION_HEIGHT_REGTEST)
