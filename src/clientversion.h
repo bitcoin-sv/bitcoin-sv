dd3516d2b7633019afe2d46c0d5ff00e6090cfdc
Richard Mills <noreply@nchain.com>
2020-01-10 09:44:52 +0000

    Update (C) year and beta version.

--- src/clientversion.h
+++ src/clientversion.h
@@ -27,7 +27,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2019
+#define COPYRIGHT_YEAR 2020
 
 #endif // HAVE_CONFIG_H
 
