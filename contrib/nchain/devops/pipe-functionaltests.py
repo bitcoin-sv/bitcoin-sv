6999dc9288f08481cf94eeeac38f956fc3e55b44
Peter Foster <p.foster@nchain.com>
2020-02-21 08:21:30 +0000

    pipe-functionaltests.py edited online with Bitbucket. Return literal result from the test runner. CORE-640

--- contrib/nchain/devops/pipe-functionaltests.py
+++ contrib/nchain/devops/pipe-functionaltests.py
@@ -21,7 +21,7 @@ def main():
         exit(-1)
 
     print("functional tests completed with result code {}".format(r1))
-    r1 ^= 1 # 1 rather than 0 is returned on success
+
     exit(abs(r1))
 
 if __name__ == '__main__':
