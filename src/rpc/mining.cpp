3618403222906f118dfd2758cfc7d9e5052d1c8b
Ziga Kokelj <ziga.kokelj@crea.si>
2020-03-20 11:52:07 +0100

    Prevent node from mining a block with coinbase transaction that creates P2SH output(with submitminingsolution, submitblock), but still treat such blocks coming from other peers as valid

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -17,6 +17,7 @@
 #include "mining/factory.h"
 #include "net.h"
 #include "policy/policy.h"
+#include "primitives/transaction.h"
 #include "pow.h"
 #include "rpc/blockchain.h"
 #include "rpc/server.h"
@@ -164,6 +165,11 @@ UniValue generateBlocks(const Config &config,
 
         std::shared_ptr<const CBlock> shared_pblock =
             std::make_shared<const CBlock>(*pblock);
+
+        if (shared_pblock->vtx[0]->HasP2SHOutput()) {
+            throw JSONRPCError(RPC_TRANSACTION_REJECTED, "bad-txns-vout-p2sh");
+        }
+
         if (!ProcessNewBlock(config, shared_pblock, true, nullptr)) {
             throw JSONRPCError(RPC_INTERNAL_ERROR,
                                "ProcessNewBlock, block not accepted");
@@ -822,6 +828,10 @@ static UniValue submitblock(const Config &config,
     if (!DecodeHexBlk(block, request.params[0].get_str())) {
         throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Block decode failed");
     }
+
+     if (blockptr->vtx[0]->HasP2SHOutput()) {
+        throw JSONRPCError(RPC_TRANSACTION_REJECTED, "bad-txns-vout-p2sh");
+     }
 
     auto submitBlock = [](const Config& config , const std::shared_ptr<CBlock>& blockptr)
     {
