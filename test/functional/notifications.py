10ef5cdcd32ea10c450721e786254775d9ab73bf
Aljaz Noe <aljaz.noe@crea.si>
2020-03-30 11:36:28 +0200

    avoid race condition in notifications functional test by writing to different files

--- test/functional/notifications.py
+++ test/functional/notifications.py
@@ -4,6 +4,7 @@
 # Distributed under the Open BSV software license, see the accompanying file LICENSE.
 """Test the -blocknotify and -walletnotify options."""
 import os
+import shutil
 
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import assert_equal, wait_until, connect_nodes_bi
@@ -14,48 +15,50 @@ class NotificationsTest(BitcoinTestFramework):
         self.setup_clean_chain = True
 
     def setup_network(self):
-        self.block_filename = os.path.join(self.options.tmpdir, "blocks.txt")
-        self.tx_filename = os.path.join(self.options.tmpdir, "transactions.txt")
+        self.dir_notify_block = os.path.join(self.options.tmpdir, "notify_block")
+        self.dir_notify_tx = os.path.join(self.options.tmpdir, "notify_tx")
+        os.mkdir(self.dir_notify_block)
+        os.mkdir(self.dir_notify_tx)
 
         # -blocknotify on node0, walletnotify on node1
-        self.extra_args = [["-blocknotify=echo %%s >> %s" % self.block_filename],
+        self.extra_args = [["-blocknotify=echo b > {}".format(os.path.join(self.dir_notify_block, '%s'))],
                            ["-rescan",
-                            "-walletnotify=echo %%s >> %s" % self.tx_filename]]
+                            "-walletnotify=echo t > {}".format(os.path.join(self.dir_notify_tx, '%s'))]]
         super().setup_network()
 
     def run_test(self):
         self.log.info("test -blocknotify")
         block_count = 10
         blocks = self.nodes[1].generate(block_count)
+        
+        # wait at most 10 seconds for expected number of block notification files
+        wait_until(lambda: len(os.listdir(self.dir_notify_block)) == block_count, timeout=10)
 
-        # wait at most 10 seconds for expected file size before reading the content
-        wait_until(lambda: os.path.isfile(self.block_filename) and os.stat(self.block_filename).st_size >= (block_count * 65), timeout=10)
-
-        # file content should equal the generated blocks hashes
-        with open(self.block_filename, 'r') as f:
-            assert_equal(sorted(blocks), sorted(f.read().splitlines()))
-
+        # file names in notify_block directory should equal the generated blocks hashes
+        assert_equal(sorted(blocks), sorted(os.listdir(self.dir_notify_block)))
+        
         self.log.info("test -walletnotify")
-        # wait at most 10 seconds for expected file size before reading the content
-        wait_until(lambda: os.path.isfile(self.tx_filename) and os.stat(self.tx_filename).st_size >= (block_count * 65), timeout=10)
+        # wait at most 10 seconds for expected number of transaction notification files
+        wait_until(lambda: len(os.listdir(self.dir_notify_tx)) == block_count, timeout=10)
 
-        # file content should equal the generated transaction hashes
+        # file names in notify_tx directory should equal the generated transaction hashes
         txids_rpc = list(map(lambda t: t['txid'], self.nodes[1].listtransactions("*", block_count)))
-        with open(self.tx_filename, 'r') as f:
-            assert_equal(sorted(txids_rpc), sorted(f.read().splitlines()))
-        os.remove(self.tx_filename)
+        assert_equal(sorted(txids_rpc), sorted(os.listdir(self.dir_notify_tx)))
 
+        shutil.rmtree(self.dir_notify_tx)
+        os.mkdir(self.dir_notify_tx)
+        
         self.log.info("test -walletnotify after rescan")
         # restart node to rescan to force wallet notifications
         self.restart_node(1)
         connect_nodes_bi(self.nodes, 0, 1)
 
-        wait_until(lambda: os.path.isfile(self.tx_filename) and os.stat(self.tx_filename).st_size >= (block_count * 65), timeout=10)
+        # wait at most 10 seconds for expected number of transaction notification files
+        wait_until(lambda: len(os.listdir(self.dir_notify_tx)) == block_count, timeout=10)
 
-        # file content should equal the generated transaction hashes
+        # file names in notify_tx directory should equal the generated transaction hashes
         txids_rpc = list(map(lambda t: t['txid'], self.nodes[1].listtransactions("*", block_count)))
-        with open(self.tx_filename, 'r') as f:
-            assert_equal(sorted(txids_rpc), sorted(f.read().splitlines()))
+        assert_equal(sorted(txids_rpc), sorted(os.listdir(self.dir_notify_tx)))
 
 if __name__ == '__main__':
     NotificationsTest().main()
