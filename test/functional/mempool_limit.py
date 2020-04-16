bbeed894c0fc9953e1ddfd196f9f997f6025d3d3
Gregor Zorec <gregor.zorec@crea.si>
2020-04-16 08:15:53 +0200

    Fixes minimum mempool size that should be at least 30% of default mempool size. If the -maxmempool size is less than the minimum, then the node will not start. Update mempool_limit functional test due to new minimal mempool limitation. Removed -maxmempool parameter from bsv-genesis-journal-reorg.py because this parameter has no effect on the test results. It was redundant before (max used mempool capacity is 132144B, which is less than 1MB). In rawtransactions.py bigger transactions are now used to fill up memory pool.

--- test/functional/mempool_limit.py
+++ test/functional/mempool_limit.py
@@ -1,53 +1,78 @@
 #!/usr/bin/env python3
-# Copyright (c) 2014-2016 The Bitcoin Core developers
-# Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# Copyright (c) 2020 Bitcoin Association
+# Distributed under the Open BSV software license, see the accompanying file LICENSE.
 
 # Test mempool limiting together/eviction with the wallet
 
-from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import *
+# 1. Send transaction (cca 5MB) with smaller fee rate (0.00000399 BSV/kB).
+# 2. Send 29 big transactions (cca 10MB per one) with higher fee rate (0.00000999 BSV/kB).
+# 3. Send another transaction with the same size as the first one and higher fee rate (0.00001999 BSV/kB).
+# 4. Mempool is full when the last transaction arrives to the mempool - the first one should be evicted (replaced with the last one) because of the smaller fee rate.
 
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import create_confirmed_utxos, satoshi_round, bytes_to_hex_str, assert_equal
+import decimal
+
+def send_tx_with_data(node, utxo, fee, data_size):
+    send_value = utxo['amount'] - fee
+    inputs = []
+    inputs.append({"txid": utxo["txid"], "vout": utxo["vout"]})
+    outputs = {}
+    addr = node.getnewaddress()
+    outputs[addr] = satoshi_round(send_value)
+    outputs["data"] = bytes_to_hex_str(bytearray(data_size))
+    rawTxn = node.createrawtransaction(inputs, outputs)
+    signedTxn = node.signrawtransaction(rawTxn)["hex"]
+
+    return node.sendrawtransaction(signedTxn)
 
 class MempoolLimitTest(BitcoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 1
-
-        self.extra_args = [["-maxmempool=5", "-spendzeroconfchange=0"]]
+        self.extra_args = [["-maxmempool=300", "-spendzeroconfchange=0", "-genesisactivationheight=1", "-maxtxsizepolicy=0"]]
 
     def run_test(self):
-        txouts = gen_return_txouts()
         relayfee = self.nodes[0].getnetworkinfo()['relayfee']
+        utxos = create_confirmed_utxos(relayfee, self.nodes[0], 40)
+        total_number_of_transactions = 30
 
-        txids = []
-        utxos = create_confirmed_utxos(relayfee, self.nodes[0], 91)
+        # create a mempool transaction that will be evicted (smaller fee rate)
+        # size: 5000211B, fee: 2000000 satoshi (0.02 BSV) --> fee rate: 0.399 sat/byte which is 0.00000399 BSV/kB
+        small_fee = decimal.Decimal('0.02')
+        small_data_size = 5000000
+        firstTxId = send_tx_with_data(self.nodes[0], utxos.pop(), small_fee, small_data_size)
 
-        # create a mempool tx that will be evicted
-        us0 = utxos.pop()
-        inputs = [{"txid": us0["txid"], "vout": us0["vout"]}]
-        outputs = {self.nodes[0].getnewaddress(): 0.0001}
-        tx = self.nodes[0].createrawtransaction(inputs, outputs)
-        # specifically fund this tx with low fee
-        self.nodes[0].settxfee(relayfee)
-        txF = self.nodes[0].fundrawtransaction(tx)
-        # return to automatic fee selection
-        self.nodes[0].settxfee(0)
-        txFS = self.nodes[0].signrawtransaction(txF['hex'])
-        txid = self.nodes[0].sendrawtransaction(txFS['hex'])
+        assert(firstTxId in self.nodes[0].getrawmempool())
+        self.log.info("First transaction %s successfully accepted to mempool.", firstTxId)
 
-        relayfee = self.nodes[0].getnetworkinfo()['relayfee']
-        base_fee = relayfee * 400
-        for i in range(3):
-            txids.append([])
-            txids[i] = create_lots_of_big_transactions(
-                self.nodes[0], txouts, utxos[30 * i:30 * i + 30], 30, (i + 1) * base_fee)
+        # transactions with higher fee rate
+        # size: 10000211B, fee: 10000000 satoshi (0.1 BSV) --> fee rate: 0.999 sat/byte which is 0.00000999 BSV/kB
+        big_fee = decimal.Decimal('0.1')
+        big_data_size = 10000000
+        for i in range(total_number_of_transactions - 1):
+            send_tx_with_data(self.nodes[0], utxos.pop(), big_fee, big_data_size)
 
-        # by now, the tx should be evicted, check confirmation state
-        assert(txid not in self.nodes[0].getrawmempool())
-        txdata = self.nodes[0].gettransaction(txid)
+        assert_equal(len(self.nodes[0].getrawmempool()), total_number_of_transactions)
+        self.log.info("%d big transactions successfully arrived to mempool.", total_number_of_transactions - 1)
+
+        # At this point, mempool size is something more than 295 000 000 bytes.
+        # If we send another transaction with size more than 5 MB and the highest fee rate, it should be replaced with the first one.
+
+        # transaction with the highest fee rate, the same size as the first one
+        # size: 5000211B, fee: 10000000 satoshi (0.1 BSV) --> fee rate: 1.999 sat/byte which is 0.00001999 BSV/kB
+        lastTxId = send_tx_with_data(self.nodes[0], utxos.pop(), big_fee, small_data_size)
+
+        # by now, the first transaction should be evicted, check confirmation state
+        assert(firstTxId not in self.nodes[0].getrawmempool())
+        # last transaction should be in mempool because it has the highest fee
+        assert(lastTxId in self.nodes[0].getrawmempool())
+
+        self.log.info("First transaction %s evicted from mempool. Last sent transaction %s successfully accepted to mempool.", firstTxId, lastTxId)
+        assert_equal(len(self.nodes[0].getrawmempool()), total_number_of_transactions)
+
+        txdata = self.nodes[0].gettransaction(firstTxId)
         assert(txdata['confirmations'] == 0)  # confirmation should still be 0
-
 
 if __name__ == '__main__':
     MempoolLimitTest().main()
