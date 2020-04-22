9ecdde718d3c86b38054ecc320d8424f5ea28f58
Simon Liu <s.liu@nchain.com>
2020-04-21 22:51:31 -0700

    Add test that stale addresses are considered IsTerrible()

--- src/test/addrman_tests.cpp
+++ src/test/addrman_tests.cpp
@@ -389,6 +389,14 @@ BOOST_AUTO_TEST_CASE(addrman_getaddr) {
     BOOST_CHECK(vAddr.size() == 461);
     // (Addrman.size() < number of addresses added) due to address collisons.
     BOOST_CHECK(addrman.size() == 2007);
+
+    // Test 26: IsTerrible() is true for stale address > ADDRMAN_HORIZON_DAYS
+    CAddress addr6 = CAddress(ResolveService("252.254.5.6", 8333), NODE_NONE);
+    int64_t nNow = GetAdjustedTime();
+    addr6.nTime = nNow - (ADDRMAN_HORIZON_DAYS * 24 * 60 * 60) - 1;
+    addrman.Add(addr6, source1);
+    CAddrInfo *info6 = addrman.Find(addr6);
+    BOOST_ASSERT(info6->IsTerrible(nNow));
 }
 
 BOOST_AUTO_TEST_CASE(caddrinfo_get_tried_bucket) {
