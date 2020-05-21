4dcc25346ba4d200f19c6de057ba2cc714cdd9cc
Bozo Dragojevic <bozo.dragojevic@crea.si>
2020-05-21 15:56:32 +0200

    Duplicate UpdateMempoolForReorg into AddTo and RemoveFrom variants
    No logic changes, just make fAddToMempool a const inside each function.
    Adjust comment for each to make it easier to understand.

--- src/validation.cpp
+++ src/validation.cpp
@@ -2096,23 +2096,96 @@ static void PostValidationStepsForFinalisedTxn(
 }
 
 /**
- * Make mempool consistent after a reorg, by re-adding or recursively erasing
+ * Make mempool consistent after a reorg, by re-adding
  * disconnected block transactions from the mempool, and also removing any other
  * transactions from the mempool that are no longer valid given the new
- * tip/height.
+ * tip/height, most notably coinbase transactions and their descendants.
  *
  * Note: we assume that disconnectpool only contains transactions that are NOT
  * confirmed in the current chain nor already in the mempool (otherwise,
  * in-mempool descendants of such transactions would be removed).
- *
- * Passing fAddToMempool=false will skip trying to add the transactions back,
- * and instead just erase from the mempool as needed.
  */
-static void UpdateMempoolForReorg(const Config &config,
-                                  DisconnectedBlockTransactions &disconnectpool,
-                                  bool fAddToMempool,
-                                  const CJournalChangeSetPtr& changeSet) {
+static void AddToMempoolForReorg(const Config &config,
+    DisconnectedBlockTransactions &disconnectpool,
+    const CJournalChangeSetPtr& changeSet) {
     AssertLockHeld(cs_main);
+    const bool fAddToMempool = true;
+    TxInputDataSPtrVec vTxInputData {};
+    // disconnectpool's insertion_order index sorts the entries from oldest to
+    // newest, but the oldest entry will be the last tx from the latest mined
+    // block that was disconnected.
+    // Iterate disconnectpool in reverse, so that we add transactions back to
+    // the mempool starting with the earliest transaction that had been
+    // previously seen in a block.
+    auto it = disconnectpool.queuedTx.get<insertion_order>().rbegin();
+    while (it != disconnectpool.queuedTx.get<insertion_order>().rend()) {
+        bool fRemoveRecursive { !fAddToMempool || (*it)->IsCoinBase() };
+        if (fRemoveRecursive) {
+            // If the transaction doesn't make it in to the mempool, remove any
+            // transactions that depend on it (which would now be orphans).
+            mempool.RemoveRecursive(**it, changeSet, MemPoolRemovalReason::REORG);
+        } else {
+            vTxInputData.emplace_back(
+                std::make_shared<CTxInputData>(
+                    TxIdTrackerWPtr{}, // TxIdTracker is not used during reorgs
+                    *it,              // a pointer to the tx
+                    TxSource::reorg,  // tx source
+                    TxValidationPriority::normal,  // tx validation priority
+                    GetTime(),        // nAcceptTime
+                    false));          // fLimitFree
+        }
+        ++it;
+    }
+    disconnectpool.queuedTx.clear();
+    // Validate a set of transactions
+    g_connman->getTxnValidator()->processValidation(vTxInputData, changeSet, true);
+    // Mempool related updates
+    std::vector<uint256> vHashUpdate {};
+    for (const auto& txInputData : vTxInputData) {
+        auto const& txid = txInputData->GetTxnPtr()->GetId();
+        if (mempool.Exists(txid)) {
+            // A set of transaction hashes from a disconnected block re-added to the mempool.
+            vHashUpdate.emplace_back(txid);
+        } else {
+            // If the transaction doesn't make it in to the mempool, remove any
+            // transactions that depend on it (which would now be orphans).
+            mempool.RemoveRecursive(*(txInputData->GetTxnPtr()), changeSet, MemPoolRemovalReason::REORG);
+        }
+    }
+    // Validator/addUnchecked all assume that new mempool entries have
+    // no in-mempool children, which is generally not true when adding
+    // previously-confirmed transactions back to the mempool.
+    // UpdateTransactionsFromBlock finds descendants of any transactions in the
+    // disconnectpool that were added back and cleans up the mempool state.
+    LogPrint(BCLog::MEMPOOL, "Update transactions from block\n");
+    mempool.UpdateTransactionsFromBlock(vHashUpdate, changeSet);
+    // We also need to remove any now-immature transactions
+    LogPrint(BCLog::MEMPOOL, "Removing any now-immature transactions\n");
+    const CBlockIndex& tip = *chainActive.Tip();
+    mempool.
+        RemoveForReorg(
+            config,
+            *pcoinsTip,
+            changeSet,
+            tip,
+            StandardNonFinalVerifyFlags(IsGenesisEnabled(config, tip.nHeight)));
+
+    // Check mempool & journal
+    mempool.CheckMempool(*pcoinsTip, changeSet);
+
+    // Mempool is now consistent. Synchronize with journal.
+    changeSet->apply();
+}
+
+/**
+* Make mempool consistent after a reorg, by recursively erasing
+* disconnected block transactions from the mempool
+*/
+static void RemoveFromMempoolForReorg(const Config &config,
+    DisconnectedBlockTransactions &disconnectpool,
+    const CJournalChangeSetPtr& changeSet) {
+    AssertLockHeld(cs_main);
+    const bool fAddToMempool = false;
     TxInputDataSPtrVec vTxInputData {};
     // disconnectpool's insertion_order index sorts the entries from oldest to
     // newest, but the oldest entry will be the last tx from the latest mined
@@ -4620,7 +4693,7 @@ static bool ActivateBestChainStep(
             if (!DisconnectTip(config, state, &disconnectpool, changeSet)) {
                 // This is likely a fatal error, but keep the mempool consistent,
                 // just in case. Only remove from the mempool in this case.
-                UpdateMempoolForReorg(config, disconnectpool, false, changeSet);
+                RemoveFromMempoolForReorg(config, disconnectpool, changeSet);
                 return false;
             }
             fBlocksDisconnected = true;
@@ -4680,7 +4753,7 @@ static bool ActivateBestChainStep(
                         // ...).
                         // Make the mempool consistent with the current tip, just in
                         // case any observers try to use it before shutdown.
-                        UpdateMempoolForReorg(config, disconnectpool, false, changeSet);
+                        RemoveFromMempoolForReorg(config, disconnectpool, changeSet);
                         return false;
                     }
                 } else {
@@ -4703,7 +4776,7 @@ static bool ActivateBestChainStep(
 
             // If any blocks were disconnected, disconnectpool may be non empty. Add
             // any disconnected transactions back to the mempool.
-            UpdateMempoolForReorg(config, disconnectpool, true, changeSet);
+            AddToMempoolForReorg(config, disconnectpool, changeSet);
         }
 
         mempool.CheckMempool(*pcoinsTip, changeSet);
@@ -4717,7 +4790,7 @@ static bool ActivateBestChainStep(
         if(fBlocksDisconnected)
         {
             LogPrintf("Exception caught during ActivateBestChainStep; updating mempool\n");
-            UpdateMempoolForReorg(config, disconnectpool, true, changeSet);
+            AddToMempoolForReorg(config, disconnectpool, changeSet);
         }
         throw;
     }
@@ -5112,7 +5185,7 @@ bool InvalidateBlock(const Config &config, CValidationState &state,
             {
                 // It's probably hopeless to try to make the mempool consistent
                 // here if DisconnectTip failed, but we can try.
-                UpdateMempoolForReorg(config, disconnectpool, false, changeSet);
+                RemoveFromMempoolForReorg(config, disconnectpool, changeSet);
                 return false;
             }
         }
@@ -5126,7 +5199,7 @@ bool InvalidateBlock(const Config &config, CValidationState &state,
 
     // DisconnectTip will add transactions to disconnectpool; try to add these
     // back to the mempool.
-    UpdateMempoolForReorg(config, disconnectpool, true, changeSet);
+    AddToMempoolForReorg(config, disconnectpool, changeSet);
 
     // The resulting new best tip may not be in setBlockIndexCandidates anymore,
     // so add it again.
