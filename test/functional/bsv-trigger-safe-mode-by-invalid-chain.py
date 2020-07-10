8bdc4ae9fd3ee463fd01b30c9c654df7df2a41c6
Boštjan Vižin <bostjan.vizin@crea.si>
2020-07-10 14:30:09 +0200

    After sending invalid block we should wait for it to be processed by waiting for reject.

--- test/functional/bsv-trigger-safe-mode-by-invalid-chain.py
+++ test/functional/bsv-trigger-safe-mode-by-invalid-chain.py
@@ -84,7 +84,11 @@ class TriggerSafeModeByIvalidChain(BitcoinTestFramework):
             # send contents of first block of second branch
             # this block is invalid and should invalidate whole second branch
             conn2.send_message(msg_block(branch_2_blocks[0]))
-            
+
+            # make sure that block is processed before doing any aserts by waiting for reject
+            # we cannot use sync_with_ping here because we sent invalid block and connection will be banned and closed
+            conn2.cb.wait_for_reject()
+
             # active tip should still be from branch 1 and branch 2 should be invalid
             wait_for_tip(conn1, branch_1_blocks[-1].hash)
             wait_for_tip_status(conn1, branch_2_blocks[-1].hash, "invalid")
