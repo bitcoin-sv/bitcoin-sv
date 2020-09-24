22ec0f6c1d291c72d05519c332d1adfa67b757d0
Domen Vrankar <domen.vrankar@crea.si>
2020-09-24 14:19:06 +0200

    Write preferring upgradable shared mutex

--- src/test/testutil.h
+++ src/test/testutil.h
@@ -8,8 +8,28 @@
 #ifndef BITCOIN_TEST_TESTUTIL_H
 #define BITCOIN_TEST_TESTUTIL_H
 
+#include <chrono>
+
 #include "fs.h"
 
 fs::path GetTempPath();
+
+// Wait for a certain amount of time and continuously call provided callback
+// until the callback returns success or the waiting time is used up
+template<typename T>
+bool wait_for(T callback, std::chrono::milliseconds duration)
+{
+    auto start = std::chrono::steady_clock::now();
+
+    while(duration > (std::chrono::steady_clock::now() - start))
+    {
+        if(callback())
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 #endif // BITCOIN_TEST_TESTUTIL_H
