718146466595ec6da40a3214706b984184e39402
Ziga Kokelj <ziga.kokelj@crea.si>
2020-09-30 09:12:51 +0200

    Rename CValidationInterface::InvalidTxMessage to InvalidTxMessageZMQ

--- src/validationinterface.cpp
+++ src/validationinterface.cpp
@@ -24,7 +24,7 @@ void RegisterValidationInterface(CValidationInterface *pwalletIn) {
     g_signals.BlockChecked.connect( boost::bind(&CValidationInterface::BlockChecked, pwalletIn, _1, _2));
     g_signals.ScriptForMining.connect(boost::bind(&CValidationInterface::GetScriptForMining, pwalletIn, _1));
     g_signals.NewPoWValidBlock.connect(boost::bind( &CValidationInterface::NewPoWValidBlock, pwalletIn, _1, _2));
-    g_signals.InvalidTxMessage.connect(boost::bind( &CValidationInterface::InvalidTxMessage, pwalletIn, _1));
+    g_signals.InvalidTxMessageZMQ.connect(boost::bind( &CValidationInterface::InvalidTxMessageZMQ, pwalletIn, _1));
 }
 
 void UnregisterValidationInterface(CValidationInterface *pwalletIn) {
@@ -40,7 +40,7 @@ void UnregisterValidationInterface(CValidationInterface *pwalletIn) {
     g_signals.BlockDisconnected.disconnect( boost::bind(&CValidationInterface::BlockDisconnected, pwalletIn, _1));
     g_signals.UpdatedBlockTip.disconnect(boost::bind( &CValidationInterface::UpdatedBlockTip, pwalletIn, _1, _2, _3));
     g_signals.NewPoWValidBlock.disconnect(boost::bind( &CValidationInterface::NewPoWValidBlock, pwalletIn, _1, _2));
-    g_signals.InvalidTxMessage.disconnect(boost::bind( &CValidationInterface::InvalidTxMessage, pwalletIn, _1));
+    g_signals.InvalidTxMessageZMQ.disconnect(boost::bind( &CValidationInterface::InvalidTxMessageZMQ, pwalletIn, _1));
 }
 
 void UnregisterAllValidationInterfaces() {
@@ -56,5 +56,5 @@ void UnregisterAllValidationInterfaces() {
     g_signals.BlockDisconnected.disconnect_all_slots();
     g_signals.UpdatedBlockTip.disconnect_all_slots();
     g_signals.NewPoWValidBlock.disconnect_all_slots();
-    g_signals.InvalidTxMessage.disconnect_all_slots();
+    g_signals.InvalidTxMessageZMQ.disconnect_all_slots();
 }
