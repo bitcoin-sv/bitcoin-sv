8491da4ae300a8cb634f4c8b49f3502c95485e1a
Domen Vrankar <domen.vrankar@crea.si>
2021-01-22 10:33:23 +0100

    Move CDiskBlockPos to a separate file

--- src/chain.h
+++ src/chain.h
@@ -8,6 +8,7 @@
 
 #include "arith_uint256.h"
 #include "consensus/params.h"
+#include "disk_block_pos.h"
 #include "pow.h"
 #include "primitives/block.h"
 #include "tinyformat.h"
@@ -134,44 +135,6 @@ public:
         if (nTimeIn > nTimeLast) {
             nTimeLast = nTimeIn;
         }
-    }
-};
-
-struct CDiskBlockPos {
-    int nFile;
-    unsigned int nPos;
-
-    ADD_SERIALIZE_METHODS
-
-    template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream &s, Operation ser_action) {
-        READWRITE(VARINT(nFile));
-        READWRITE(VARINT(nPos));
-    }
-
-    CDiskBlockPos() { SetNull(); }
-
-    CDiskBlockPos(int nFileIn, unsigned int nPosIn) {
-        nFile = nFileIn;
-        nPos = nPosIn;
-    }
-
-    friend bool operator==(const CDiskBlockPos &a, const CDiskBlockPos &b) {
-        return (a.nFile == b.nFile && a.nPos == b.nPos);
-    }
-
-    friend bool operator!=(const CDiskBlockPos &a, const CDiskBlockPos &b) {
-        return !(a == b);
-    }
-
-    void SetNull() {
-        nFile = -1;
-        nPos = 0;
-    }
-    bool IsNull() const { return (nFile == -1); }
-
-    std::string ToString() const {
-        return strprintf("CBlockDiskPos(nFile=%i, nPos=%i)", nFile, nPos);
     }
 };
 
