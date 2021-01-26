2cd49f5aa55f8b8d14dc92d5cfa6d62464516c82
Richard Mills <r.mills@nchain.com>
2021-01-26 08:16:01 +0000

    Bump zeromq Gitian version to 4.3.4

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -1,9 +1,8 @@
 package=zeromq
-$(package)_version=4.3.2
+$(package)_version=4.3.4
 $(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=ebd7b5c830d6428956b67a0454a7f8cbed1de74b3b01e5c33c5378e22740f763
-$(package)_patches=0001-fix-build-with-mingw-thread-name-fix.patch 0002-no-void-pointer-to-store-function-pointer.patch
+$(package)_sha256_hash=c593001a89f5a85dd2ddf564805deb860e02471171b3f204944857336295c3e5
 
 define $(package)_set_vars
   $(package)_config_opts=--without-docs --disable-shared --without-libsodium --disable-curve --disable-curve-keygen --disable-perf --disable-Werror
@@ -12,8 +11,6 @@ define $(package)_set_vars
 endef
 
 define $(package)_preprocess_cmds
-   patch -p1 < $($(package)_patch_dir)/0001-fix-build-with-mingw-thread-name-fix.patch && \
-   patch -p1 < $($(package)_patch_dir)/0002-no-void-pointer-to-store-function-pointer.patch && \
    cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub config
 endef
 
