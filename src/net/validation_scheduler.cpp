85b2af4be81d55864e66a553cd2b55f6a3b783a1
Bojan Blazica <bojan.blazica@crea.si>
2021-06-07 11:10:46 +0200

    Use curly brace init.

--- src/net/validation_scheduler.cpp
+++ src/net/validation_scheduler.cpp
@@ -23,9 +23,9 @@ void DrawGraph(const TxInputDataSPtrVec& txs);
 
 ValidationScheduler::ValidationScheduler(CThreadPool<CDualQueueAdaptor> &threadPool,
                                          TxInputDataSPtrVec &txs, TypeValidationFunc func)
-        : validationFunc(std::move(func)),
-          txs(txs),
-          validatorThreadPool(threadPool),
+        : validationFunc{std::move(func)},
+          txs{txs},
+          validatorThreadPool{threadPool},
           MAX_TO_SCHEDULE{threadPool.getPoolSize() * MAX_TO_SCHEDULE_FACTOR}
 {
     // Initialize status for each transaction and build mapping from TxId to position.
