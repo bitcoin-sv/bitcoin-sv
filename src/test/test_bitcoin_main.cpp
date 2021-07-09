d7cbf685f27f62374bc69139b9585d64f996fa81
Bozo Dragojevic <bozo.dragojevic@crea.si>
2021-07-09 18:21:27 +0200

    Add commandline option --enable-logging to unit tests.

--- src/test/test_bitcoin_main.cpp
+++ src/test/test_bitcoin_main.cpp
@@ -5,3 +5,28 @@
 #define BOOST_TEST_MODULE Bitcoin Test Suite
 
 #include <boost/test/unit_test.hpp>
+#include "logging.h"
+#include <algorithm>
+
+bool HasCustomOption(std::string option)
+{
+    const auto& argc = boost::unit_test::framework::master_test_suite().argc;
+    const auto& argv = boost::unit_test::framework::master_test_suite().argv;
+    return std::any_of(argv, argv+argc, [&option](auto& arg) {return option == arg;});
+}
+
+struct EnableLoggingFixture {
+    EnableLoggingFixture() {
+        std::string option {"--enable-logging"};
+        if (HasCustomOption(option)) {
+            GetLogger().EnableCategory(BCLog::ALL);
+            GetLogger().fPrintToConsole = true;
+            GetLogger().fLogTimeMicros = true;
+            GetLogger().fLogTimestamps = true;
+        } else {
+            BOOST_TEST_MESSAGE("To enable logging, run the unit tests with   -- " << option);
+        }
+    }
+};
+
+BOOST_TEST_GLOBAL_FIXTURE(EnableLoggingFixture);
