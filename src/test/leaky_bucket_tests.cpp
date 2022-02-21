e8d56aff4b76d696211e07951d32b38d7a5022aa
Richard Mills <r.mills@nchain.com>
2022-02-21 07:23:10 +0000

    Allow us to specify the drain amount per interval for a leaky bucket.
    Allowing the user to also specify the amount we drain per interval as well
    as the interval size makes for a much more flexible class and one which
    is easier to use.

--- src/test/leaky_bucket_tests.cpp
+++ src/test/leaky_bucket_tests.cpp
@@ -12,34 +12,47 @@ BOOST_AUTO_TEST_SUITE(LeakyBucketTests)
 
 BOOST_AUTO_TEST_CASE(FillDrain)
 {
+    // Bucket that drains 1 every ms
     using namespace std::chrono_literals;
-    LeakyBucket<std::chrono::milliseconds> bucket { 1000, 1ms };
+    LeakyBucket<std::chrono::milliseconds> bucket1 { 1000, 1ms };
+
+    // Bucket that drains 2 every ms, expressed in terms of seconds
+    LeakyBucket<std::chrono::milliseconds> bucket2 { 1000, 1s, 2000 };
 
     // Check we're not overflowing at start
-    BOOST_CHECK(!bucket.Overflowing());
-    BOOST_CHECK_EQUAL(bucket.GetFillLevel(), 0);
+    BOOST_CHECK(!bucket1.Overflowing());
+    BOOST_CHECK_EQUAL(bucket1.GetFillLevel(), 0);
+    BOOST_CHECK(!bucket2.Overflowing());
+    BOOST_CHECK_EQUAL(bucket2.GetFillLevel(), 0);
 
     // Part fill
-    BOOST_CHECK(!(bucket += 500));
-    BOOST_CHECK(bucket.GetFillLevel() > 0);
+    BOOST_CHECK(!(bucket1 += 500));
+    BOOST_CHECK(bucket1.GetFillLevel() > 0);
+    BOOST_CHECK(!(bucket2 += 500));
+    BOOST_CHECK(bucket2.GetFillLevel() > 0);
 
     // Check we drain at something like the correct rate
-    size_t startLevel { bucket.GetFillLevel() };
+    double startLevel1 { bucket1.GetFillLevel() };
+    double startLevel2 { bucket2.GetFillLevel() };
     std::this_thread::sleep_for(5ms);
-    BOOST_CHECK(bucket.GetFillLevel() < startLevel);
-    BOOST_CHECK(bucket.GetFillLevel() > 0);
+    BOOST_CHECK(bucket1.GetFillLevel() < startLevel1);
+    BOOST_CHECK(bucket1.GetFillLevel() > 0);
+    BOOST_CHECK(bucket2.GetFillLevel() < startLevel2);
+    BOOST_CHECK(bucket2.GetFillLevel() > 0);
+    BOOST_CHECK(bucket2.GetFillLevel() < bucket1.GetFillLevel());
     std::this_thread::sleep_for(500ms);
-    BOOST_CHECK_EQUAL(bucket.GetFillLevel(), 0);
+    BOOST_CHECK_EQUAL(bucket1.GetFillLevel(), 0);
+    BOOST_CHECK_EQUAL(bucket2.GetFillLevel(), 0);
 
     // Check overflow
-    BOOST_CHECK(!(bucket += 1000));
-    BOOST_CHECK(bucket += 1000);
-    BOOST_CHECK(bucket.Overflowing());
+    BOOST_CHECK(!(bucket1 += 1000));
+    BOOST_CHECK(bucket1 += 1000);
+    BOOST_CHECK(bucket1.Overflowing());
 
     // Test creating partially filled
-    LeakyBucket<std::chrono::milliseconds> bucket2 { 1000, 500, 1ms };
-    BOOST_CHECK(bucket2.GetFillLevel() > 0);
-    BOOST_CHECK(!bucket2.Overflowing());
+    LeakyBucket<std::chrono::milliseconds> bucket3 { 1000, 500, 1ms };
+    BOOST_CHECK(bucket3.GetFillLevel() > 0);
+    BOOST_CHECK(!bucket3.Overflowing());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
