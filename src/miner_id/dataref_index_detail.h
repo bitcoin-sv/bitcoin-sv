5993aa533b51bff5c9ebe4a1b25dc5b1227b1fc1
Patrick Fromberg <p.fromberg@nchain.com>
2022-08-24 20:32:05 +0100

    copyright

--- src/miner_id/dataref_index_detail.h
+++ src/miner_id/dataref_index_detail.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Bitcoin Association
+// Copyright (c) 2022 Bitcoin Association
 // Distributed under the Open BSV software license, see the accompanying file LICENSE.
 
 #pragma once
