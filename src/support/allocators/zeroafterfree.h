26ff0a5402d7a905f015d64b38bca3574c8e42dd
Richard Mills <r.mills@nchain.com>
2023-02-07 10:29:39 +0000

    Fix C++20 compilation errors

--- src/support/allocators/zeroafterfree.h
+++ src/support/allocators/zeroafterfree.h
@@ -13,15 +13,7 @@
 
 template <typename T>
 struct zero_after_free_allocator : public std::allocator<T> {
-    // MSVC8 default copy constructor is broken
     typedef std::allocator<T> base;
-    typedef typename base::size_type size_type;
-    typedef typename base::difference_type difference_type;
-    typedef typename base::pointer pointer;
-    typedef typename base::const_pointer const_pointer;
-    typedef typename base::reference reference;
-    typedef typename base::const_reference const_reference;
-    typedef typename base::value_type value_type;
     zero_after_free_allocator() throw() {}
     zero_after_free_allocator(const zero_after_free_allocator &a) throw()
         : base(a) {}
