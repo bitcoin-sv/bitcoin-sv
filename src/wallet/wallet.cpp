b957378efe942c0c912a05e03944800d7442b9cc
Chris Gibson <c.gibson@nchain.com>
2023-07-18 10:06:51 +0000

    set pwalletdbEncryption to null after deletion

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -807,6 +807,7 @@ bool CWallet::EncryptWallet(const SecureString &strWalletPassphrase) {
         if (!EncryptKeys(vMasterKey)) {
             pwalletdbEncryption->TxnAbort();
             delete pwalletdbEncryption;
+            pwalletdbEncryption = nullptr;
             // We now probably have half of our keys encrypted in memory, and
             // half not... die and let the user reload the unencrypted wallet.
             assert(false);
@@ -817,6 +818,7 @@ bool CWallet::EncryptWallet(const SecureString &strWalletPassphrase) {
 
         if (!pwalletdbEncryption->TxnCommit()) {
             delete pwalletdbEncryption;
+            pwalletdbEncryption = nullptr;
             // We now have keys encrypted in memory, but not on disk... die to
             // avoid confusion and let the user reload the unencrypted wallet.
             assert(false);
