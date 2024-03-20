53095fcd1deb96216f6f5a0ab05ef004ffca2542
Chris Gibson <c.gibson@nchain.com>
2024-03-20 18:07:15 +0000

    benchmark.sh fix

--- .github/workflows/benchmark/benchmark.sh
+++ .github/workflows/benchmark/benchmark.sh
@@ -1,19 +1,19 @@
 # Copyright (c) 2024 BSV Blockchain Association
     
 lscpu | awk '
-/Socket\(s\):/ {
-    printf(" %d", $2)
+/Thread\(s\) per core:/{
+    printf("%d", $4)
 }
 
 /Core\(s\) per socket:/ {
     printf(" %d", $4)
 }
 
-/Thread\(s\) per core:/{
-    printf("%d", $4)
+/Socket\(s\):/ {
+    printf(" %d", $2)
 }
 ' | {
-    read sockets cps tpc
+    read tpc cps sockets
     threads=$((sockets * cps * tpc))
     printf 'Sockets: %d\nCores/Socket: %d\nThreads/Core: %d\nThreads: %d\n\n' \
             $sockets \
