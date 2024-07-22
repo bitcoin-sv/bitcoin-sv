d419ae9902403fb1e72de7486db9d3cdb7535f56
Chris Gibson <c.gibson@nchain.com>
2024-07-22 15:24:43 +0000

    OP_3DUP bench test

---
+++ src/bench/op_codes.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2024 BSV Blockchain Association
+// Distributed under the Open BSV software license, see the accompanying file
+// LICENSE.
+
+#include "bench.h"
+
+#include "config.h"
+#include "script/interpreter.h"
+#include "script/opcodes.h"
+#include "script/script_error.h"
+#include "taskcancellation.h"
+
+#include <cstdint>
+
+static void op_dup(benchmark::State& state)
+{
+    using namespace std;
+
+    const Config& config = GlobalConfig::GetConfig();
+    const int32_t flags{SCRIPT_UTXO_AFTER_GENESIS};
+    const vector<uint8_t> script{ OP_1, 2, 0xff, 0x7f, OP_NUM2BIN,
+                                  OP_DUP,
+                                  OP_2DUP,
+                                  OP_3DUP, OP_3DUP, OP_3DUP, OP_3DUP, OP_3DUP,
+                                  OP_3DUP, OP_3DUP, OP_3DUP, OP_3DUP, OP_3DUP};
+    while(state.KeepRunning())
+    {
+        auto source = task::CCancellationSource::Make();
+        LimitedStack stack{INT64_MAX};
+        const auto status = EvalScript(config,
+                                       false,
+                                       source->GetToken(),
+                                       stack,
+                                       CScript{script.begin(), script.end()},
+                                       flags,
+                                       BaseSignatureChecker{});
+        assert(status);
+        assert(status->index() == 1);
+    }
+}
+BENCHMARK(op_dup);
+
