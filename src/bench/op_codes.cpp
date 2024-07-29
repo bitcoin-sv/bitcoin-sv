62d5880168a33d7247cbbf79eab53d8696d43059
Chris Gibson <c.gibson@nchain.com>
2024-07-29 16:35:51 +0000

    OP_SPLIT bench test

--- src/bench/op_codes.cpp
+++ src/bench/op_codes.cpp
@@ -69,3 +69,33 @@ static void op_2rot(benchmark::State& state)
 }
 BENCHMARK(op_2rot);
 
+static void op_split(benchmark::State& state)
+{
+    using namespace std;
+
+    const Config& config = GlobalConfig::GetConfig();
+    const int32_t flags{SCRIPT_UTXO_AFTER_GENESIS};
+    const vector<uint8_t> script{ OP_1, 3, 0xff, 0xff, 0x7f, OP_NUM2BIN,
+                                        3, 0xff, 0xff, 0x3f, OP_SPLIT,
+                                        3, 0xff, 0xff, 0x1f, OP_SPLIT,
+                                        3, 0xff, 0xff, 0x0f, OP_SPLIT,
+                                        3, 0xff, 0xff, 0x07, OP_SPLIT,
+                                        3, 0xff, 0xff, 0x03, OP_SPLIT,
+                                        3, 0xff, 0xff, 0x01, OP_SPLIT };
+    while(state.KeepRunning())
+    {
+        auto source = task::CCancellationSource::Make();
+        LimitedStack stack{INT64_MAX};
+        const auto status = EvalScript(config,
+                                       false,
+                                       source->GetToken(),
+                                       stack,
+                                       CScript{script.begin(), script.end()},
+                                       flags,
+                                       BaseSignatureChecker{});
+        assert(status);
+        assert(status->index() == 1);
+    }
+}
+BENCHMARK(op_split);
+
