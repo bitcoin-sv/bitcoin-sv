d0ce75cdf2ab5604e2397ea636759b1a97853a88
Chris Gibson <c.gibson@teranode.group>
2025-01-17 11:12:25 +0000

    CHDChain

--- src/wallet/walletdb.h
+++ src/wallet/walletdb.h
@@ -62,18 +62,21 @@ enum DBErrors {
 /* simple HD chain data model */
 class CHDChain {
 public:
-    uint32_t nExternalChainCounter;
-    uint32_t nInternalChainCounter;
+    uint32_t nExternalChainCounter{};
+    uint32_t nInternalChainCounter{};
     //!< master key hash160
     CKeyID masterKeyID;
 
     static const int VERSION_HD_BASE = 1;
     static const int VERSION_HD_CHAIN_SPLIT = 2;
     static const int CURRENT_VERSION = VERSION_HD_CHAIN_SPLIT;
-    int nVersion;
+    int nVersion{CHDChain::CURRENT_VERSION};
 
-    // NOLINTNEXTLINE (cppcoreguidelines-pro-type-member-init)
-    CHDChain() { SetNull(); }
+    CHDChain()
+    {
+        masterKeyID.SetNull();
+    }
+
     ADD_SERIALIZE_METHODS
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream &s, Operation ser_action) {
@@ -83,13 +86,6 @@ public:
         if (this->nVersion >= VERSION_HD_CHAIN_SPLIT) {
             READWRITE(nInternalChainCounter);
         }
-    }
-
-    void SetNull() {
-        nVersion = CHDChain::CURRENT_VERSION;
-        nExternalChainCounter = 0;
-        nInternalChainCounter = 0;
-        masterKeyID.SetNull();
     }
 };
 
