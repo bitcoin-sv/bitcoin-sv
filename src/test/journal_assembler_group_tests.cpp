833661704a64f8f90ea723217d3f8e5b06c0140c
Chris Gibson <c.gibson@teranode.group>
2025-01-29 16:30:44 +0000

    Fix clang-tidy *narrowing-conversions in journal_assembler tests

--- src/test/journal_assembler_group_tests.cpp
+++ src/test/journal_assembler_group_tests.cpp
@@ -44,7 +44,8 @@ namespace
         txn.vout.resize(1);
         std::vector<uint8_t> stuff;
         stuff.resize(txnSize - 32); // make serialized transaction 500 bytes
-        txn.vout[0].scriptPubKey = CScript() << stuff << OP_DROP << unique++ << OP_DROP; // NOLINT(bugprone-narrowing-conversions, cppcoreguidelines-narrowing-conversions)
+        // NOLINTNEXTLINE(*-narrowing-conversions)
+        txn.vout[0].scriptPubKey = CScript() << stuff << OP_DROP << unique++ << OP_DROP;
         const auto tx = MakeTransactionRef(std::move(txn));
         return JournalEntryAccess::Make(
             std::make_shared<CTransactionWrapper>(tx, nullptr),
