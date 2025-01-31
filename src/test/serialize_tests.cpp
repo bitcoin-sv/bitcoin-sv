0e4760b8979434074e06da8de4dafa98e7c338ad
Chris Gibson <c.gibson@teranode.group>
2025-01-31 11:22:35 +0000

    Remove redundant clang-tidy bugprone-sizeof-expression marker

--- src/test/serialize_tests.cpp
+++ src/test/serialize_tests.cpp
@@ -467,7 +467,7 @@ BOOST_AUTO_TEST_CASE(class_methods) {
     BOOST_CHECK(methodtest3 == methodtest4);
 
     CDataStream ss2(SER_DISK, PROTOCOL_VERSION, intval, boolval, stringval,
-                    FLATDATA(charstrval), txval); // NOLINT(bugprone-sizeof-expression)
+                    FLATDATA(charstrval), txval);
     ss2 >> methodtest3;
     BOOST_CHECK(methodtest3 == methodtest4);
 }
