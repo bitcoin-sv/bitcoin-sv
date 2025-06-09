6dfd6e9d2934512dcd507a9b0f6ac7a99e66fdd2
Chris Gibson <c.gibson@teranode.group>
2025-06-09 10:00:27 +0000

    add cmake ENABLE_COVERAGE option for cmake with gcc

--- .github/actions/build_bitcoin_cmake/build.sh
+++ .github/actions/build_bitcoin_cmake/build.sh
@@ -2,23 +2,27 @@
 # Copyright (c) 2023 Bitcoin Association Distributed under the Open BSV
 # software license, see the accompanying file LICENSE
 
-BUILD_TYPE=${1,,}
+TOOLSET=${1,,}
+BUILD_TYPE=${2,,}
 
 args=()
 args+=(-DCMAKE_BUILD_TYPE="${BUILD_TYPE}")
 args+=(-DCMAKE_CXX_FLAGS=-Werror\ -fno-omit-frame-pointer)
-args+=(-DENABLE_CLANG_TIDY=ON)
+
+if [[ $TOOLSET == clang ]]; then
+    args+=(-DENABLE_CLANG_TIDY=ON)
+fi
 
 if [[ $BUILD_TYPE == debug ]];
 then
     args+=(-Denable_debug=ON)
 fi
 
-for option in ${@:2}
+for option in ${@:3}
 do
     case ${option,,} in
         asan) args+=(-Denable_asan=ON); args+=(-DCRYPTO_USE_ASM=OFF) ;;
-        cov)  args+=(-D ENABLE_COVERAGE=ON) ;;
+        cov)  args+=(-DENABLE_COVERAGE=ON) ;;
         tsan) args+=(-Denable_tsan=ON) ;;
         usan) args+=(-Denable_ubsan=ON) ;;
     esac
--- contrib/generate_coverage.sh
+++ contrib/generate_coverage.sh
@@ -6,48 +6,61 @@
 
 set -euo pipefail
 
-llvm_path="/usr/lib/llvm-19/bin"
+PROGRAM=${0##*/}
 
 function Display_Help
 {
     cat <<EoH
 
-Uses llvm-profdata and llvm-cov to generate coverage reports for the specified binary.
-Presumes that the binary has been compiled with coverage instrumentation and that 
-default.profraw file exists in the current directory.
+    Produces html coverage reports for a given binary using lcov or llvm-cov.
 
-    usage: $PROGRAM executable
-
-    e.g. "$PROGRAM test_bitcoin"
+    usage e.g.
+      "$PROGRAM -t gcc test_bitcoin"
 
     Options:
+      -t = Toolset [clang|gcc] (default: clang)
       -h = Show this help message and exit
 EoH
     exit 0
 }
 
-PROGRAM=${0##*/}
-BINARY=$1
+(($# < 1)) && Display_Help
+TOOLSET=clang
 
-while getopts 'h' VAL; do
+while getopts 'ht:' VAL; do
     case $VAL in
         h ) Display_Help;;
+        t ) TOOLSET=$OPTARG;;
     esac
 done
 shift $((OPTIND -1))
+    
+(($# < 1)) && Display_Help
+BINARY=$1
 
-(($# != 1)) && Display_Help
+if [ $TOOLSET = 'gcc' ]; then
+    lcov --capture \
+         --directory . \
+         --exclude='/usr/*' \
+         -o coverage.lcov
 
-FILE=default.profraw
-if [[ ! -f "$FILE" ]]; then
-    echo "'$FILE' does not exist."
-    exit 1
-elif [[ ! -s "$FILE" ]]; then
-    echo "'$FILE' is empty."
+elif [ $TOOLSET = 'clang' ]; then
+    FILE=default.profraw
+    if [[ ! -f "$FILE" ]]; then
+        echo "'$FILE' does not exist."
+        exit 1
+    elif [[ ! -s "$FILE" ]]; then
+        echo "'$FILE' is empty."
+        exit 1
+    fi
+
+    llvm_path="/usr/lib/llvm-19/bin"
+    $llvm_path/llvm-profdata merge -sparse default.profraw -o default.profdata
+    $llvm_path/llvm-cov export --format=lcov -instr-profile=default.profdata $BINARY > coverage.lcov
+    
+else
+    echo "$TOOLSET is not a valid toolset. Use 'clang' or 'gcc'."
     exit 1
 fi
 
-$llvm_path/llvm-profdata merge -sparse default.profraw -o default.profdata
-$llvm_path/llvm-cov show -instr-profile=default.profdata $BINARY > ${BINARY}_show.txt
-$llvm_path/llvm-cov report -instr-profile=default.profdata $BINARY > ${BINARY}_report.txt
-
+genhtml coverage.lcov --output-directory ${BINARY}_cov
