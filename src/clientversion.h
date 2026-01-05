7b9499ad004f4344374883299cd26d1dd9b1445c
Richard Mills <r.mills@teranode.group>
2026-01-05 11:05:01 +0000

    Bump (C) year to 2026

--- src/clientversion.h
+++ src/clientversion.h
@@ -27,7 +27,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2025
+#define COPYRIGHT_YEAR 2026
 
 #endif // HAVE_CONFIG_H
 
