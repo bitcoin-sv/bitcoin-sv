4994a2d491371102e540fd5844a04d4f0f08eb16
Chris Gibson <c.gibson@teranode.group>
2026-04-16 13:07:40 +0000

    remove unused CDynamicCriticalSection

--- src/sync.h
+++ src/sync.h
@@ -106,7 +106,6 @@ public:
     }
 };
 
-typedef CCriticalSection CDynamicCriticalSection;
 /** Wrapped boost mutex: supports waiting but not recursive locking */
 typedef AnnotatedMixin<boost::mutex> CWaitableCriticalSection;
 
